706dec96153ab5bd149d591c7295187b2ac22299
gclin <t96994024@gs.ncku.edu.tw>
2026-09-19 15:24:47 +0800

    CH1-L4: OWASP Top 10

--- internal/httpserver/middleware.go
+++ internal/httpserver/middleware.go
@@ -29,6 +29,13 @@ func applyMiddleware(handler http.Handler, middlewareChain ...middleware) http.H
 	return handler
 }
 
+func nosniffMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(responseWriter http.ResponseWriter, request *http.Request) {
+		responseWriter.Header().Set("X-Content-Type-Options", "nosniff")
+		next.ServeHTTP(responseWriter, request)
+	})
+}
+
 func permissiveCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(responseWriter http.ResponseWriter, request *http.Request) {
 		if origin := request.Header.Get("Origin"); origin != "" {
