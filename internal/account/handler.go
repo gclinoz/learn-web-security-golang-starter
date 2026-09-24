ef5ce765615fa8abc51b6f5da988173e4366c8b4
gclinoz <t96994024@gs.ncku.edu.tw>
2026-09-24 14:32:52 +0800

    CH2-L12: Reauthentication

--- internal/account/handler.go
+++ internal/account/handler.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/bootdotdev/learn-web-security/internal/accounts"
 	"github.com/bootdotdev/learn-web-security/internal/auth/mfa"
+	"github.com/bootdotdev/learn-web-security/internal/auth/passwords"
 	"github.com/bootdotdev/learn-web-security/internal/auth/sessions"
 	"github.com/bootdotdev/learn-web-security/internal/httpx"
 	"github.com/bootdotdev/learn-web-security/internal/logging"
@@ -78,9 +79,18 @@ func (handler *Handler) UpdateEmail(responseWriter http.ResponseWriter, request 
 	if !ok || !handler.verifyCSRF(responseWriter, request, current.Session.CSRFToken) {
 		return
 	}
+	pwd, passwordErr := httpx.FormValue(request, "currentPassword")
 	email, emailErr := httpx.FormValue(request, "email")
-	if emailErr != nil {
+	if passwordErr != nil || emailErr != nil {
 		handler.errorPage(responseWriter, http.StatusBadRequest, "Invalid Request", "The submitted form is invalid.")
+		return
+	}
+	valid := passwords.Verify(pwd, current.User.PasswordHash)
+	if pwd == "" || !valid {
+		err := handler.renderPage(responseWriter, http.StatusForbidden, current, "Re-enter your current password to change your email.")
+		if err != nil {
+			handler.internalError(responseWriter, request, err)
+		}
 		return
 	}
 	email = accounts.NormalizeEmail(email)
